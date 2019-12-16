6727dbd32e639c90c1a0448e219063dc36b4ca25
Jamar Torres <Jetorres88@gmail.com>
2019-12-15 23:44:03 -0500

    solved recursive problem for counting times that th appears in a string

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,27 +3,27 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+# ths = "th"
+
 def count_th(word):
-    
-    # TBC
-    
     # pass
 
-    if word == "":
-        print('word', word)
-        print(0)
+    if len(word) <= 1:
+        # print(f'No th\'s included {0}')
         return 0
 
-    elif word == "th":
-        return 1
-
-    # else: 
-
-
-    return word
+    elif word[0] + word[1] == 'th':
+        print(f'number of times word has th: {1 + count_th(word[1:])} ')
+        return 1 + count_th(word[1:])
+    else: 
+        return count_th(word[1:])
 
 
-print(count_th(''))
+
+
+# print(count_th(''))
+# print(count_th('th'))
 print(count_th('th'))
+print(count_th('thtjtjthth'))
 
 
